a4ed1c699c30d43df1a392308dac11079f8f445a
Aditya <75035219+pegvin@users.noreply.github.com>
2022-09-10 15:47:16 +0530

    Separate Code

--- src/main.c
+++ src/main.c
@@ -44,55 +44,12 @@
 #include <time.h>
 #include <unistd.h>
 
+#include "main.h"
 #include "theme.h"
 #include "colors.h"
 #include "assets.h"
 
 theme_t* EdTheme = NULL;
-
-/*** defines ***/
-
-#if ARU_BUILD_STABLE == 0
-	#define ARU_BUILD_TYPE "dev"
-#else
-	#define ARU_BUILD_TYPE "stable"
-#endif
-
-#ifndef ARU_VERSION
-	#define ARU_VERSION "0.0.0"
-#endif
-
-#define ARU_TAB_STOP 4
-#define ARU_QUIT_TIMES 3
-
-#define CTRL_KEY(k) ((k) & 0x1f)
-
-enum editorKey {
-	BACKSPACE = 127,
-	ARROW_LEFT = 1000,
-	ARROW_RIGHT,
-	ARROW_UP,
-	ARROW_DOWN,
-	DEL_KEY,
-	HOME_KEY,
-	END_KEY,
-	PAGE_UP,
-	PAGE_DOWN
-};
-
-enum editorHighlight {
-	HL_NORMAL = 0,
-	HL_COMMENT,
-	HL_MLCOMMENT,
-	HL_KEYWORD1,
-	HL_KEYWORD2,
-	HL_STRING,
-	HL_NUMBER,
-	HL_MATCH
-};
-
-#define HL_HIGHLIGHT_NUMBERS (1<<0)
-#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 /*** data ***/
 
@@ -104,33 +61,6 @@ struct editorSyntax {
 	char *multiline_comment_start;
 	char *multiline_comment_end;
 	int flags;
-};
-
-typedef struct erow {
-	int idx;
-	int size;
-	int rsize;
-	char *chars;
-	char *render;
-	unsigned char *hl;
-	int hl_open_comment;
-} erow;
-
-struct editorConfig {
-	int cx, cy;
-	int rx;
-	int rowoff;
-	int coloff;
-	int screenrows;
-	int screencols;
-	int numrows;
-	erow *row;
-	int dirty;
-	char *filename;
-	char statusmsg[80];
-	time_t statusmsg_time;
-	struct editorSyntax *syntax;
-	struct termios orig_termios;
 };
 
 struct editorConfig E;
---
+++ src/main.h
@@ -0,0 +1,78 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <termios.h>
+#include <time.h>
+
+// Defines
+
+#if ARU_BUILD_STABLE == 0
+	#define ARU_BUILD_TYPE "dev"
+#else
+	#define ARU_BUILD_TYPE "stable"
+#endif
+
+#ifndef ARU_VERSION
+	#define ARU_VERSION "0.0.0"
+#endif
+
+#define ARU_TAB_STOP 4
+#define ARU_QUIT_TIMES 3
+#define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+// Structs & Enums
+enum editorKey {
+	BACKSPACE = 127,
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
+};
+
+enum editorHighlight {
+	HL_NORMAL = 0,
+	HL_COMMENT,
+	HL_MLCOMMENT,
+	HL_KEYWORD1,
+	HL_KEYWORD2,
+	HL_STRING,
+	HL_NUMBER,
+	HL_MATCH
+};
+
+typedef struct erow {
+	int idx;
+	int size;
+	int rsize;
+	char *chars;
+	char *render;
+	unsigned char *hl;
+	int hl_open_comment;
+} erow;
+
+struct editorConfig {
+	int cx, cy;
+	int rx;
+	int rowoff;
+	int coloff;
+	int screenrows;
+	int screencols;
+	int numrows;
+	erow *row;
+	int dirty;
+	char *filename;
+	char statusmsg[80];
+	time_t statusmsg_time;
+	struct editorSyntax *syntax;
+	struct termios orig_termios;
+};
+
+#endif
