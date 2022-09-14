b3ea9f0367a805f348fd80d704faf36a366f7979
Aditya <75035219+pegvin@users.noreply.github.com>
2022-09-14 23:03:38 +0530

    Update abuf.h

--- src/abuf.h
+++ src/abuf.h
@@ -2,13 +2,13 @@
 #define ABUG_H 1
 
 typedef struct {
-	char *buffer;
-	int len;
+	char* buffer; // Actual buffer which stores data
+	int len;      // Size of the buffer above
 } abuf_t;
 
-#define ABUF_INIT {NULL, 0}
+#define ABUF_INIT { NULL, 0 }
 
-void abAppend(abuf_t *ab, const char *s, int len);
-void abFree(abuf_t *ab);
+void abAppend(abuf_t* ab, const char* s, int len);
+void abFree(abuf_t* ab);
 
 #endif
