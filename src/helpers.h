d6a73b9bad46b50f3a0ad48e8c2d938b5ff6e433
Aditya <75035219+pegvin@users.noreply.github.com>
2022-09-14 23:05:49 +0530

    Update helpers.h

--- src/helpers.h
+++ src/helpers.h
@@ -1,6 +1,7 @@
 #ifndef HELPERS_H
 #define HELPERS_H 1
 
+bool str_startswith(const char *prefix, const char *str);
 char* _strdup(const char *str);
 void strLower(char* str, int len);
 char* formatBytes(int bytes);
